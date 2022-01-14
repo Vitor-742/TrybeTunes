f504274e5361f8956902c7b35dd0d545927aa20f
Vitor Campos <vitorcampos742@gmail.com>
2022-01-14 17:04:11 -0300

    requisito 6

---
+++ src/components/CardAlbum.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class CardAlbum extends React.Component {
+  render() {
+    const { album } = this.props;
+    const { artistName, artworkUrl100, collectionName, collectionId } = album;
+    return (
+      <Link
+        to={ `album/${collectionId}` }
+        data-testid={ `link-to-album-${collectionId}` }
+      >
+        <div key={ collectionId }>
+          <img src={ artworkUrl100 } alt={ collectionName } />
+          <h3>{ collectionName }</h3>
+          <p>{ artistName }</p>
+        </div>
+      </Link>
+    );
+  }
+}
+
+CardAlbum.propTypes = {
+  album: PropTypes.shape({
+    artistName: PropTypes.string.isRequired,
+    artworkUrl100: PropTypes.string.isRequired,
+    collectionName: PropTypes.string.isRequired,
+    collectionId: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default CardAlbum;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,39 +1,81 @@
 import React from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import CardAlbum from '../components/CardAlbum';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       inputButton: true,
+      artistName: '',
+      loading: false,
+      albums: [],
+      empty: false,
     };
   }
+
+  procuraAlbum = () => {
+    this.setState({ loading: true }, async () => {
+      const { artistName } = this.state;
+      const albums = await searchAlbumsAPI(artistName);
+      const a = albums.length === 0
+        ? this.setState({ empty: true })
+        : this.setState({ empty: false });
+      this.setState({
+        albums,
+        loading: false,
+      });
+      return a;
+    });
+  };
 
   habilitaBotao = (event) => {
     const a = event.target.value.length >= 2
       ? this.setState({ inputButton: false })
       : this.setState({ inputButton: true });
+    this.setState({ artistName: event.target.value });
     return a;
-  }
+  };
 
   render() {
-    const { inputButton } = this.state;
+    const { inputButton, artistName, loading, albums, empty } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <div>
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            onChange={ this.habilitaBotao }
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ inputButton }
-          >
-            Pesquisar
-          </button>
+          {loading ? (
+            <Loading />
+          ) : (
+            <div>
+              <input
+                type="text"
+                data-testid="search-artist-input"
+                onChange={ this.habilitaBotao }
+              />
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ inputButton }
+                onClick={ this.procuraAlbum }
+              >
+                Pesquisar
+              </button>
+            </div>
+          )}
+          {albums.length !== 0 && (
+            <div>
+              <p>
+                Resultado de álbuns de:
+                {` ${artistName}`}
+              </p>
+              {albums.map((album) => (
+                <CardAlbum album={ album } key={ album.collectionId } />
+              ))}
+            </div>
+          )}
+          {empty === true && <p>Nenhum álbum foi encontrado</p>}
         </div>
       </div>
     );
