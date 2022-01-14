925aeee96b407c9a637db41263d66935abb4710b
Vitor Campos <vitorcampos742@gmail.com>
2022-01-14 12:20:14 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,39 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputButton: true,
+    };
+  }
+
+  habilitaBotao = (event) => {
+    const a = event.target.value.length >= 2
+      ? this.setState({ inputButton: false })
+      : this.setState({ inputButton: true });
+    return a;
+  }
+
   render() {
+    const { inputButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <div>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.habilitaBotao }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ inputButton }
+          >
+            Pesquisar
+          </button>
+        </div>
       </div>
     );
   }
